c027adddde97785185bbbd0ea48b14adf124f7d1
richter <richter@gfz-potsdam.de>
2013-11-30 13:59:40 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 from numpy.distutils.core import Extension, setup
 
-VERSION='0.1.1'
+VERSION='0.1.2'
 
 with open('README.rst') as f:
     README = f.read()
 if not 'dev' in VERSION: # get image for correct version from travis-ci
     README = README.replace('branch=master', 'branch=v%s' % VERSION)
+DESCRIPTION = README.split('\n')[2]
+LONG_DESCRIPTION = '\n'.join(README.split('\n')[5:])
 
 ext = Extension(name='toeplitz',
                 sources=['src/toeplitz.pyf', 'src/toeplitz.f90'])
 
 setup(name='toeplitz',
       version=VERSION,
-      description='Wrapper for fortran 90 toeplitz package',
-      long_description=README,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
       author='Tom Richter',
       author_email='richter@gfz-potsdam.de',
       license='MIT',
       url='https://github.com/trichter/toeplitz',
       ext_modules=[ext],
       scripts=['scripts/toeplitz-runtests'],
-      requires=['numpy']
+      requires=['numpy'],
+      include_package_data=True
       )
